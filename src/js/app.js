f416dc4993e96e3f04601c932996c1e43aa354e8
chr1st0pher <chris.halden@gmail.com>
2019-07-05 15:35:24 +0100

    _

--- src/js/app.js
+++ src/js/app.js
@@ -7,10 +7,13 @@ function loaded(d){
         SetPage('home');
         $('.footer').show();
         if(window.location.hash!==''){
-          console.log(window.location.hash.substr(1,7));
           if(window.location.hash.substr(1,7)=='Session'){
             SetPage('training');
             setTrainingContent(window.location.hash.substr(8,1))
+          }
+          if(window.location.hash.substr(1,8)=='Exercise'){
+            SetPage('practice');
+            setTrainingContent(window.location.hash.substr(9,1))
           }
         }
     }
